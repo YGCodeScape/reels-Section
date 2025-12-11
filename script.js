0940ed12814f399eae23ae9a0c59a0963f0494b7
yashG <yrg.creation.ai@gmail.com>
2025-12-11 18:53:28 +0530

    css doe

---
+++ script.js
@@ -0,0 +1,22 @@
+let videos = [ 
+    {
+        videoSrc : "./Assets/video01.mp4",
+    },
+    {
+        videoSrc : "./Assets/video02.mp4",
+    },
+    {
+        videoSrc : "./Assets/video03.mp4",
+    },
+    {
+        videoSrc : "./Assets/video04.mp4",
+    }
+]
+
+const videoElement = document.getElementById("video")
+
+const videoSRC = videoElement.src
+
+function changeVideo() {
+    
+}
