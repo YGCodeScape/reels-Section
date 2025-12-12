7010679e495c1407822a5267d04293729dee8b54
yashG <yrg.creation.ai@gmail.com>
2025-12-12 20:42:30 +0530

    updating

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const followBtns = document.querySelectorAll(".follow-btn");
+const likeIcon = document.querySelectorAll("#like-btn")
 
-followBtns.forEach(btn => {
+function followLogic() {
+    followBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         btn.classList.toggle("active");
         btn.innerText = btn.classList.contains("active") ? "Followed" : "Follow";
     });
 });
+
+likeIcon.forEach(like => {
+    like.addEventListener("click",() => {
+        console.log("liked");
+    })
+})
+}
+followLogic()
+
